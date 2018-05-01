61643b519dd4f5ac7f97dd0fbc77afd7b2dcb92b
Setor Agodo <setor.k.agodo@gmail.com>
2018-05-01 04:08:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,9 @@
-var playlists {
+var playlist {
   "Michael Jackson": "Thriller",
   "Gucci Mane": "Money Make ya Handsome"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['Michael Jackson'] = 'Thriller'
+  playlist['Gucci Mane'] = 'Money Make ya Handsome'  
 }
